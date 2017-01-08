96655081f94006735583671ba7f7bfd58edd4508
Jianyi Gao 高健一 <JianyiGao@gaojianyis-MacBook-Air.local>
2017-01-08 18:05:17 -0500

    Optional features 2/4 completed

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -10,36 +10,44 @@ import UIKit
 import AFNetworking
 import MBProgressHUD
 
-
 let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
 var url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
 
-
-
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var movieSearchBar: UISearchBar!
+    
     var movies: [NSDictionary]?
+    var filteredData: [NSDictionary]?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        movieSearchBar.delegate = self
+        
         
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), forControlEvents: UIControlEvents.ValueChanged)
         tableView.insertSubview(refreshControl, atIndex: 0)
-        
-        
-        let request = NSURLRequest(URL: url!)
+
         let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
+        let request = NSURLRequest(URL: url!)
+        
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        
         let task:NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {(dataOrNil, response, error) in
+           
+            MBProgressHUD.hideHUDForView(self.view, animated: true)
+            
             if let data = dataOrNil {
                 if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary {
-                    print("response: \(responseDictionary)")
                     
                     self.movies = responseDictionary["results"] as! [NSDictionary]
+                    self.filteredData = self.movies
                     self.tableView.reloadData()
                 }
             }
@@ -47,7 +55,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         task.resume()
         
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,11 +63,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
-
-    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        if let movies = movies {
-            return movies.count
+        if let filteredMovies  = filteredData {
+            return filteredMovies.count
         } else {
             return 0
         }
@@ -68,45 +74,36 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
-        let movie = movies![indexPath.row]
+        let movie = filteredData![indexPath.row]
         let title = movie["title"] as! String
+        
         let overview = movie["overview"] as! String
         
-        let baseUrl = "http://image.tmdb.org/t/p/w500"
-        let posterPath = movie["poster_path"] as! String
-        let imageUrl = NSURL(string: baseUrl + posterPath)
-        
+        if  let posterPath = movie["poster_path"] as? String {
+            
+            let baseURL = "http://image.tmdb.org/t/p/w500"
+            
+            let imageURL = NSURL(string: baseURL + posterPath)
+            
+            cell.posterView.setImageWithURL(imageURL!)
+            cell.posterView.alpha = 0
+            UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.TransitionCurlUp, animations: { () -> Void in
+                cell.posterView.alpha = 1
+                }, completion: nil)
+            
+        } else {
+            cell.posterView.image = nil
+        }
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        cell.posterView.setImageWithURL(imageUrl!)
-        
-        
-        
         
         
         return cell
-      
     }
+   
     
-    func loadDataFromNetwork(){
-        let myRequest = NSURLRequest(URL: url!)
-        let session = NSURLSession(
-            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-            delegate:nil,
-            delegateQueue:NSOperationQueue.mainQueue()
-        )
-        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-        
-        let task : NSURLSessionDataTask = session.dataTaskWithRequest(myRequest, completionHandler: {(data, response, error) in
-            
-            MBProgressHUD.hideHUDForView(self.view, animated: true)
-                                                                      
-        });
-        task.resume()
-    }
-
-        
+    
     
     func refreshControlAction(refreshControl: UIRefreshControl) {
 
@@ -125,15 +122,43 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         task.resume()
     }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filteredData = searchText.isEmpty ? movies : movies!.filter({(movie: NSDictionary) -> Bool in
+            if let title = movie["title"] as? String {
+                return title.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+            }
+            return false
+        })
+        
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.movieSearchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        movieSearchBar.showsCancelButton = false
+        movieSearchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+         
+    //override func pirepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
-    */
+    //}
+
+ 
+        
+    
+    
+    
 
 }
